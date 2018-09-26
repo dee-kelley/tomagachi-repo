1013b25a05319051753c443b47eb3e3ac9468070
Deirdre <deirdre@Deirdres-MacBook-Air.local>
2018-09-25 20:43:34 -0600

    all good

--- javascrip9-24/app9-24.js
+++ javascrip9-24/app9-24.js
@@ -1,85 +1,215 @@
 $(document).ready()
 // global variable
 //===================================================
-let seconds=0;
-let hungry = false;
-let bored = false;
-let sleepy = false;
-let $body = $('body')
+let seconds = 0;
+// let hungry = 0;
+// let bored = 0;
+// let sleepy = 0;
+// let old = 0;
+let $body = $('body');
 
-//window.alert("Toma-GOTCHA NOW BITCH")
-//Toma constructor
-//======================================================
-$('.start').on('click', (e)=> {
-    // $('button').remove();
-    $(e.currentTarget).remove();
-    console.log("clik")
+//=====================================================
+
+
+// function playAudio() { 
+//     music.play(); 
+
+// } 
+const fuckinA = $('<audio autoplay controls id="myAudio"> <source src="Mortal-Kombat.mp3"></audio>');
+$(document).on('click', ".music", function(){
+    $body.append(fuckinA)
+    $('.music').remove();
 });
 
-class Tomagachi {
-constructor(hungry, sleepy, bored) {
-    this.hungry = hungry;  //25000 seconds
-    this.bored = bored; //50000 seconds
-    this.sleepy = sleepy; //1000000 seconds
-    this.age = 0
+//==================================================
+//window.alert("Toma-GOTCHA NOW BITCH")
+//Toma constructor
+//const game = {} game.timer = setInterval clearInterval(game.timer)
+//======================================================
+
+  // First select the input
+  // Then maybe find its VALUEEEE
+
+  class Tomagachi {
+    constructor(name) {
+        this.name = name;
+        this.hungry = 0;  //25000 seconds
+        this.bored = 0; //50000 seconds
+        this.sleepy = 0; //1000000 seconds
+        this.age = 0;
+        this.alive = true
+}}
+
+const naming = prompt("Please enter your pet's name", "Name");
+let pet = new Tomagachi(naming);
+$('.name').append(naming)
+$('#pic').append('<img src = "https://78.media.tumblr.com/2a24e5730af3f37ed8d8eb54bf5105f4/tumblr_o60ghokDWV1umhm4zo2_r1_250.gif"></img')
+
+
+const stats = () =>{
+    $('.age').text(pet.age);
 }
-};
 
-tomagachi = {
-    hungry: 0,
-    bored: false,
-    sleepy: false,
-    age: 0
-} 
-$('.feed').on('click', ()=> {
+// const timePassing = () => {
+// 	updateTamagotchi();
+// 	seconds++;
+// 		if(seconds % 7 == 0) {
+// 			pet.hunger++;
+// 			$('.hungry').empty().append(pet.hunger);
+// 		}
+const timePassingFeed = setInterval (()=>{
+    seconds++;
+    if(seconds%2 ===0){
+        pet.hungry++;
+        $('.hunger').text(pet.hungry)
+        console.log(pet.hungry)
+    } 
+    // else if(pet.hungry == 10){
+    //     pet.alive = false;
+    //     clearInterval(timePassingFeed);
+    //     //pet dies - write a function for dying with a reason as parameter const die = (reason) => {clear interval $('mesg').text}
+    // }
+}, 1000)
+
+const timePassingSleep = setInterval (()=>{
+    if(seconds%2 ===0){
+        pet.sleepy++;
+        $('.sleepy').text(pet.sleepy)
+        console.log(pet.sleepy)
+        
+        }
+        // else if(pet.sleepy == 10){
+        // pet.alive = false;
+        // clearInterval(timePassingSleep);
+        // }
+}, 1000)
+
+const timePassingPlay = setInterval (()=>{
+    if(seconds%5 ===0){
+        pet.bored++
+        $('.bored').text(pet.bored)
+        //div displaying score.html(pet.bored)
+        console.log(pet.bored)
+        }}, 1000)
+    //  else if(pet.bored == 10){
+    //     pet.alive = false;
+    // clearInterval(timePassingPlay);
+    //     }
+
+
+const timePassingAge = setInterval (()=>{
+    if(seconds%3 === 0){
+        pet.age++
+        $('.age').text(pet.age)
+        //change picture
+    } if(seconds === 5){
+        alert(`${naming} IS EVOLVING!`)
+        $('#pic > img').remove();
+        $('#pic').append('<img src = "https://pa1.narvii.com/6597/d64a6d75fc3e31854691fe5c3a53bff8b6c424db_hq.gif"></img>');
+    } if(seconds === 10){
+        alert(`${naming} IS EVOLVING AGEEEEEEN!`)
+        $('#pic > img').remove();
+        $('#pic').append('<img src = "https://pa1.narvii.com/6437/9f8771e4760bed80cef05e19eb145393e4bdea00_128.gif"></img>');
+    
+    } else if(pet.age == 10 || pet.hungry == 10 || pet.bored ==10 || pet.sleepy ==10 ){
+    pet.alive = false;
+    alert(`YOU LET ${naming} DIE`);
+    $('#pic > img').remove();
+    $('header').append('<img src = "https://im5.ezgif.com/tmp/ezgif-5-2253463451.gif"></img>');
+        clearInterval(timePassingAge);
+        clearInterval(timePassingPlay);
+        clearInterval(timePassingSleep);
+        clearInterval(timePassingFeed);
+        console.log(timePassingAge)
+    }
+}, 1000);
+
+
+// $('button').on('click', () => {
+//     const $input = $('input').val();
+//     console.log($input);
+//   });
+
+
+
+$(document).on('click', ".feed", function() {
+    console.log(`THIS IS HUNGRY ${pet.hungry}`);
+	if (pet.hungry <= 1 ) {                                         //|| this.sleepy <= 1
+        alert(`I'm full, Kevin Spacey!`);
+        return
+	}
+    pet.hungry-= 2;
+        console.log(pet.hungry)
+		// updateTamagotchi();
+});
+$(document).on('click', ".sleep", function() {
+    console.log(`CLICKED WORKED ${pet.sleepy}`);
+	if (pet.sleepy <= 1) {                                      //pet.hungry <= 1 || 
+		alert(`I am awake, baka!`);
+		return
+	}
+    pet.sleepy-= 1;
+    //jquery to update page =======================================================
+
+        console.log(pet.sleepy)
+		// updateTamagotchi();
+});
+$(document).on('click', ".play", function() {
+    console.log(`THIS IS BORED ${pet.bored}`);
+	if (pet.bored <= 1) {                                               //pet.hungry <= 1 || 
+		alert(`That was fun, senpai!`);
+		return
+	}
+    pet.bored-= 1;
+        console.log(pet.bored)
+		// updateTamagotchi();
+});
+
+// $(document).on('click', ".age", function() {
+//     console.log(`THIS IS AGE ${pet.age}`)
+// 	if (pet.age = 10) {      
+//         pet.alive=false;                                            //pet.hungry <= 1 || 
+// 		alert(`Time makes you older...`);
+// 		return
+// 	}
+//     pet.age+= 1;
+//         console.log(pet.age)
+// 		// updateTamagotchi();
+// });
+
+/*$('.feed').on('click', ()=> {
+ 
+    // clearInterval(timePassingFeed);
     hungry++;
-//    <input type="button" onclick="clearInterval(timer)" value="stop">
     $('header > img').remove();
-    //setInterval.reset(timePassing)
-        $('header').append('<img src = "https://c3.legalinsurrection.com/wp-content/uploads/2015/03/Dick-Cheney-620x435.jpg"></img>')
-
+       // $('header').append('<img src = "https://c3.legalinsurrection.com/wp-content/uploads/2015/03/Dick-Cheney-620x435.jpg"></img>')
+        //$('<h2>Tomagotcha is purr-eased </h2>').appendTo($('header'));
         console.log("click")
 });
 $('.sleep').on('click', ()=> {
     sleepy++;
+    // clearInterval(timePassingSleep);
     $('header > img').remove();
-    $('header').append('<img src = "https://1.bp.blogspot.com/-TZMcOZBAMjM/WcNm69wvXaI/AAAAAAAAPY8/ksdfZy8NEx8d46N2EW0G3PUzZzBXVlslQCLcBGAs/s1600/Screenshot%2B2017-09-21%2Bat%2B2.07.24%2BAM.png"></img>')
+   // $('header').append('<img src = "https://secure.i.telegraph.co.uk/multimedia/archive/01242/cheney_1242441c.jpg"></img>')
     console.log("click")
 });
 $('.play').on('click', ()=> {
     bored++;
+    // clearInterval(timePassingPlay);
     $('header > img').remove();
-    $('header').append('<img src = "https://static01.nyt.com/images/2015/12/08/multimedia/dick-cheney-terror/dick-cheney-terror-superJumbo.jpg"></img>')
+    //$('header').append('<img src = "https://vignette.wikia.nocookie.net/clubpenguin/images/a/a7/Kermit_sprite_running.gif/revision/latest?cb=20150513171656"></img>')
 
     console.log("click")
 });
-//hungry
-// };
-// const wellBeing = () => {
-// if()
-// }
+// const music = play(()=>{
+//     const audio = document.getElementById("audio");
+//     audio.play();
+//               })
+
+//just keep hungry stat going up and hungry level will go down when you feed it
 
 
-const timePassing = setInterval (()=>{
-    seconds++;
-    if(seconds%25 ===0){
-        $body.append('<h1 class="hungry">FEED ME<h1>')
-        console.log("hungry")
-    }
-    if(seconds%45 ===0){
-        $body.append('<h1 class="bored">GIVE ME A NUKE<h1>')
-        console.log("bored")
-    }
-    if(seconds%55 ===0){
-        $body.append('<h1 class="sleepy">I AM TIRED<h1>')
-        console.log("sleepy")
-    }
-    // if(age > 100){
-    //     //change picture
-    // }
-  //   if(seconds < 10){
-  //       clearInterval(timePassing);
-  //       console.log("Done")
-    // }
-(timePassing)
-}, 1000)
+//scoreboards or messages that have to pop up
+//<h2>your pet has ( happiness)<> or time: <>  make elements the <span class=whatever>-small part of it that looks different 
+//than everything elsegrap that span and use .text
+//<h2> your happiness<span class = "happy">0</span></h2>*/
